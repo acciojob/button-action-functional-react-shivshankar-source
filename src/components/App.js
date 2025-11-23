bd3505882e47308108dbd1904be29b9af077b432
Shiv <shivshakti.ss771@gmail.com>
2025-11-23 12:42:43 +0530

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
-import { Button } from "./Button";
 
 function App() {
+  const[ispara,setIspara] = useState(false);
+  const handleclick = ()=>{
+    setIspara(!ispara);
+  }
   return (
     <div id="main">
-      <Button/>
+      {ispara && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>}
+      <button id="click" onClick={handleclick}>Click</button>
     </div>
   );
 }
 
 
-export default App;
+export default App;
